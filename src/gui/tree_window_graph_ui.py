2c43542d3e4bce96015362ce5967f7a363a960a4
Maks <mzawislak@student.agh.edu.pl>
2023-04-23 15:05:22 +0200

    interacitve not done

--- src/gui/tree_window_graph_ui.py
+++ src/gui/tree_window_graph_ui.py
@@ -7,85 +7,103 @@ from src.Classes.person import Person
 import random
 import pydot
 from networkx.drawing.nx_pydot import graphviz_layout
-import igraph as ig
-
+from graphviz import Digraph
+from src.definitions import definitions
 
 # TODO zamykanie okna powoduje zapis drzewa ?
 
-class TreeWindowGraphUi:
-    # def on_node_click(self, event):
-    #     node_text = event.artist.get_text()
-    #     node_num = str(node_text)
-    #     print(f"Kliknięto węzeł {node_num}")
+from collections import deque
+import random
+import math
+import pydot
 
+from graphviz import Digraph, Source, Graph
+
+
+class TreeWindowGraphUi:
     def setup_ui(self, person: Person):
-        G = nx.DiGraph()  # graf skierowany
-        G.add_node(str(person))
+        G =Digraph(comment='Family Tree')
+
+        # add the root node
+        G.node(str(person))
+
+        partner_edge = []
 
         already_added = set()
         queue = deque()
 
-        level = 0
         level_max = -1 * math.inf
         level_min = math.inf
-        persons_dict = {}
 
-        queue.append((person, level))
+        queue.append((person, 0))
         already_added.add(person)
 
         while len(queue) > 0:
             person_tmp, level_tmp = queue.pop()
 
-            # level_max = max(level_max,level_tmp)
-            # level_min = min(level_min,level_tmp)
-            persons_dict[str(person_tmp)] = (random.uniform(0, 1),level_tmp)
+            level_max = max(level_max, level_tmp)
+            level_min = min(level_min, level_tmp)
 
             for person_partner in person_tmp.partners:
                 if person_partner not in already_added:
                     already_added.add(person_partner)
-                    queue.append((person_partner,level_tmp))
-                G.add_node(str(person_partner))
-                G.add_edge(str(person_tmp), str(person_partner))
+                    queue.append((person_partner, level_tmp))
 
-            for person_child in person_tmp.children:
-                if person_child not in already_added:
-                    already_added.add(person_child)
-                    queue.append((person_child,level_tmp-1))
-                G.add_node(str(person_child))
-                G.add_edge(str(person_tmp), str(person_child))
+
+                if (str(person_tmp),str(person_partner)) not in partner_edge:
+                    partner_edge.append((str(person_partner),str(person_tmp)))
+                    with G.subgraph() as s:
+                        s.attr(rank='same')
+                        s.node(str(person_tmp))
+                        s.node(str(person_partner))
+                        s.edge(str(person_tmp), str(person_partner))
+                        #s.edge(str(person_partner),str(person_tmp))
+
+            if person_tmp.children != []:
+                with G.subgraph() as c:
+                    c.attr(rank='same')
+                    for person_child in person_tmp.children:
+                        c.node(str(person_child))
+
+
+                for person_child in person_tmp.children:
+                    if person_child not in already_added:
+                        already_added.add(person_child)
+                        queue.append((person_child, level_tmp - 1))
+
+
+                    G.edge(str(person_tmp), str(person_child))
 
             if person_tmp.mother is not None:
                 if person_tmp.mother not in already_added:
                     already_added.add(person_tmp.mother)
-                    queue.append((person_tmp.mother,level_tmp+1))
-                G.add_node(str(person_tmp.mother))
-                # G.add_edge(str(person_tmp), str(person_tmp.mother))
+                    queue.append((person_tmp.mother, level_tmp + 1))
+
+                G.node(str(person_tmp.mother))
 
             if person_tmp.father is not None:
                 if person_tmp.father not in already_added:
                     already_added.add(person_tmp.father)
-                    queue.append((person_tmp.father,level_tmp+1))
-                G.add_node(str(person_tmp.father))
-                # G.add_edge(str(person_tmp), str(person_tmp.father))
+                    queue.append((person_tmp.father, level_tmp + 1))
 
-        print(persons_dict)
-        print(persons_dict)
-        # pos = nx.spring_layout(G)
-        #pos = graphviz_layout(G, prog="twopi")
-        # https://stackoverflow.com/questions/57512155/how-to-draw-a-tree-more-beautifully-in-networkx
-        pos = persons_dict
+                G.node(str(person_tmp.father))
+
+        print("end bfs")
+        # render the graph
+        G.render('family_tree1.gv')
+        print("end ernder")
+        Gnet = nx.DiGraph(nx.nx_pydot.read_dot(definitions.ROOT_DIR + '\\family_tree1.gv'))
 
 
-        nx.draw_networkx_nodes(G, pos)
-        nx.draw_networkx_edges(G, pos, arrowstyle='-', arrowsize=20)
+        nx.draw(Gnet,with_labels=True)
 
         ax = plt.gca()
         ax.axis('off')
 
-        for node in G.nodes:
-            button = ax.annotate(str(node), xy=pos[node], ha="center", va="center")
-            button.set_picker(True)
-            # przycisk odpala okieno z person z danymi tej osoby, przyciski do dodania rodziny,
+        # for node in G.nodes:
+        #     button = ax.annotate(str(node), xy=pos[node], ha="center", va="center")
+        #     button.set_picker(True)
+        #     # przycisk odpala okieno z person z danymi tej osoby, przyciski do dodania rodziny,
 
         plt.subplots_adjust(hspace=0.5)
         self.fig = plt.gcf()
